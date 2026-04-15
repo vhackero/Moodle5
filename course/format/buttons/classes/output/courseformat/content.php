c1b87ecee406dd92f3cf8524210987bd2c7b543f
vhackero <vhackero@gmail.com>
2026-04-15 12:54:48 -0600

    format_buttons: use native activity list in editing mode

--- course/format/buttons/classes/output/courseformat/content.php
+++ course/format/buttons/classes/output/courseformat/content.php
@@ -168,8 +168,10 @@ class content extends content_base
         $data->arrownavigation = $this->get_arrow_navigation_data($sectionnavigation);
 
         if ($data->isarrownavigation && !empty($data->sections[0])) {
+            $isediting = $format->show_editor();
             $data->sections[0]->isarrownavigation = true;
-            $data->sections[0]->isarrowstyle = $this->should_group_current_section();
+            $data->sections[0]->isediting = $isediting;
+            $data->sections[0]->isarrowstyle = !$isediting && $this->should_group_current_section();
             if ($data->sections[0]->isarrowstyle) {
                 $data->sections[0]->arrowcolumns = $this->get_arrow_columns_data($data->sections[0]);
             }
