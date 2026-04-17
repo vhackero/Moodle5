27a63b3f0f6c2da2c0a4f7922810a59d9d398a4c
vhackero <vhackero@gmail.com>
2026-04-17 10:19:01 -0600

    Add es_mx language pack for buttons format strings

---
+++ course/format/buttons/lang/es_mx/format_buttons.php
@@ -0,0 +1,120 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     format_buttons
+ * @category    string
+ * @copyright   2023 Jhon Rangel <jrangelardila@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addsections'] = 'Agregar sección';
+$string['bgcolor'] = 'Color del botón';
+$string['bgcolor_desc'] = 'Indica el color de fondo para los botones';
+$string['bgcolor_help'] = 'Indica el color de los botones. Ejemplo: #920020';
+$string['bgcolor_selected'] = 'Color del botón seleccionado';
+$string['bgcolor_selected_desc'] = 'Fondo para el botón seleccionado';
+$string['color_gruping'] = 'Color para los botones del grupo {$a}';
+$string['colorfont'] = 'Indica el color de la fuente';
+$string['colorfont_help'] = 'Indica el color del texto de los botones. Ejemplo: #FFFFFF';
+$string['config'] = 'Configuración del complemento';
+$string['currentsection'] = 'Esta sección';
+$string['deletesection'] = 'Eliminar sección';
+$string['editsection'] = 'Editar sección';
+$string['editsectionname'] = 'Editar nombre de la sección';
+$string['eight'] = 'Ocho';
+$string['eleven'] = 'Once';
+$string['fifteen'] = 'Quince';
+$string['five'] = 'Cinco';
+$string['fontcolor'] = 'Color de fuente';
+$string['fontcolor_desc'] = 'Indica el color de la fuente para los botones';
+$string['fontcolor_selected'] = 'Color de la fuente para el botón seleccionado';
+$string['fontcolor_selected_desc'] = 'Color de la fuente para el botón seleccionado';
+$string['four'] = 'Cuatro';
+$string['fourteen'] = 'Catorce';
+$string['groups_course'] = 'Máximo de grupos';
+$string['groups_course_desc'] = 'Indique el número máximo de grupos que puede tener un curso';
+$string['hidefromothers'] = 'Ocultar sección';
+$string['mycustomstring'] = 'Puedes agregar cadenas \'extra\' a través del archivo de recetas.';
+$string['mycustomstring2'] = 'Otra cadena con el marcador {$a->some}.';
+$string['newsectionname'] = 'Nuevo nombre para la sección {$a}';
+$string['nine'] = 'Nueve';
+$string['numeretion'] = 'Numeración';
+$string['numeretion_desc'] = 'Numeración de los botones';
+$string['one'] = 'Uno';
+$string['option1'] = 'Números';
+$string['option2'] = 'Alfabeto en minúsculas';
+$string['option3'] = 'Alfabeto en mayúsculas';
+$string['option4'] = 'Números romanos';
+$string['navigationstyle'] = 'Estilo de navegación';
+$string['navigationstyle_help'] = 'Seleccione cómo se navegará entre secciones en el formato del curso.';
+$string['navigationstyle_buttons'] = 'Botones';
+$string['navigationstyle_arrows'] = 'Flechas de navegación';
+$string['showcategorybranch'] = 'Mostrar ramificación de categorías en el título del curso';
+$string['showcategorybranch_help'] = 'Si se habilita, el nombre del curso incluirá la ruta jerárquica de categorías.';
+$string['coursenamecolor'] = 'Color del nombre del curso';
+$string['coursenamecolor_help'] = 'Color usado para el nombre del curso (clickeable) en la línea de ramificación de categorías.';
+$string['showonlysectionsmenu'] = 'En estilo de flechas, mostrar solo secciones en el menú lateral';
+$string['showonlysectionsmenu_help'] = 'Oculta actividades del índice lateral y muestra solo enlaces de secciones cuando se usa el estilo de flechas.';
+$string['column_learning_modules'] = 'Módulos para actividades de aprendizaje';
+$string['column_learning_modules_help'] = 'Nombres de módulos separados por coma para la columna Actividades de aprendizaje (ejemplo: assign,quiz).';
+$string['column_support_modules'] = 'Módulos para recursos de apoyo';
+$string['column_support_modules_help'] = 'Nombres de módulos separados por coma para la columna Recursos de apoyo (ejemplo: resource,url,page).';
+$string['column_collaborative_modules'] = 'Módulos para trabajo colaborativo';
+$string['column_collaborative_modules_help'] = 'Nombres de módulos separados por coma para la columna Trabajo colaborativo (ejemplo: forum,chat,wiki).';
+$string['groupingsections'] = 'Secciones que usarán agrupación en columnas';
+$string['groupingsections_help'] = 'Números de sección o rangos separados por coma que deben usar columnas en modo flechas (ejemplo: 2-16,20).';
+$string['sectionstripeurl'] = 'Pleca de sección';
+$string['sectionstripeurl_help'] = 'URL/ruta de imagen para la pleca mostrada arriba del bloque de semana.';
+$string['column_learning_title'] = 'Actividades de aprendizaje';
+$string['column_support_title'] = 'Recursos de apoyo';
+$string['column_collaborative_title'] = 'Trabajo colaborativo';
+$string['column_uncategorized_title'] = 'Otras actividades';
+$string['noactivitiesmessage'] = 'No hay actividades de aprendizaje, recursos de apoyo o trabajo colaborativo que mostrar';
+$string['plugin_description'] = 'Buttons es un formato de curso que crea un menú con botones para acceder a las secciones, una por una.';
+$string['pluginname'] = 'Formato buttons';
+$string['rounded'] = 'Redondos';
+$string['section_zero_ubication'] = 'Mostrar la sección 0 como boton';
+$string['section_zero_ubication_help'] = 'Indicar si desea que la sección 0 se muestre como boton. De lo contrario quedara en la parte superior';
+$string['sectionname'] = 'Sección';
+$string['sections_gruping'] = 'Número de secciones para el grupo {$a}';
+$string['sections_gruping_help'] = 'Número de secciones que contiene el grupo';
+$string['selectd_file'] = 'Imagen para las secciones';
+$string['selectd_file_desc'] = 'Imagen para el inicio de las secciones, en la parte superior';
+$string['selectform'] = 'Indique la forma de los botones';
+$string['selectform_help'] = 'Seleccionar la forma en que se muestran los botones';
+$string['selectoption'] = 'Seleccionar numeración de los botones';
+$string['selectoption_help'] = 'Indica la forma de numerar los botones';
+$string['seven'] = 'Siete';
+$string['settings'] = 'Configuración por defecto';
+$string['showfromothers'] = 'Mostrar sección';
+$string['six'] = 'Seis';
+$string['sixteen'] = 'Dieciséis';
+$string['square'] = 'Cuadrados';
+$string['ten'] = 'Diez';
+$string['thirteen'] = 'Trece';
+$string['three'] = 'Tres';
+$string['title_gruping'] = 'Título del agrupamiento {$a}';
+$string['title_gruping_help'] = 'El texto se ubica antes del grupo';
+$string['title_section_view'] = 'Mostrar el titulo de cada sección';
+$string['title_section_view_help'] = 'Indique si se considera necesario mostrar el titulo de cada sección';
+$string['twelve'] = 'Doce';
+$string['two'] = 'Dos';
+$string['zero'] = 'Cero';
