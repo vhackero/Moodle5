ae66413856eac35cb9ba2cfd94263c93d108a3e2
vhackero <vhackero@gmail.com>
2026-05-06 19:00:05 -0600

    Harden recovery flow with sesskey, method checks and safe CURP lookup

--- local/qrcurp/recovery/index.php
+++ local/qrcurp/recovery/index.php
@@ -5,9 +5,13 @@ if(isloggedin()){
     redirect('/index.php');
 }
 
+$PAGE->set_url(new \moodle_url('/local/qrcurp/recovery/index.php'));
+$PAGE->set_context(\context_system::instance());
+
 echo $OUTPUT->header();
 ?>
 <form method="post" action="datos.php">
+    <input type="hidden" name="sesskey" value="<?php echo sesskey(); ?>">
     <h1>Por favor, teclea tu CURP para recuperar tus credenciales de acceso:</h1>
     <hr>
     <div class="form-row col-md-8 align-items-center">
